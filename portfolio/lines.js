0f330a8fce970b97913340b5fe438be9dbc30ef4
SarahHennessy17 <42851976+SarahHennessy17@users.noreply.github.com>
2018-12-06 17:20:34 -0500

    Add files via upload

--- portfolio/lines.js
+++ portfolio/lines.js
@@ -1,28 +1,42 @@
 var text = [
+{
+   "type": "dialouge",
+   "txt": "Thanks for coming to my page! See you next time!"
+  },
  {
    "type": "slug",
    "txt": "INT. OFFICE - DAY",
  },
  {
-   "title": "action",
+   "type": "action",
    "txt": "A designer, Sarah Hennessy, 22, a senior at Parsons School of Design waves to the reader."
   },  
  {
-   "title": "character",
+   "type": "name",
    "txt": "SARAH"
+  },
+  {
+   "type": "dialouge",
+   "txt": "Hi! Thanks for coming!"
   },
 ]
 
 // NOT USING / BACK UP 
+// function typeSlug() {
+// 	document.getElementById("slug").innerHTML += slug.charAt(i);
+	
+//     setTimeout(typeSlug, speed, typeEffect);
+// }
+
+// This is my really long text so that I can test how different type lengths look!
+
 
 // function typeWriter() {
 //     document.getElementById("text").innerHTML += txt.charAt(i);
 //     i++;
 //     setTimeout(typeWriter, speed, typeEffect);
 // }
-
-// function typeWriter() {
-//     document.getElementById("type").innerHTML += scriptPick.charAt(i);
-//     i++; 
-//     setTimeout(typeWriter, speed, typeEffect);
-// }
+    // document.getElementById("type").innerHTML += scriptPick.charAt(i);
+        // $('#type').append(scriptPick); 
+    // $('#type').append("<br>");
+    // setTimeout(typeWriter, speed, typeEffect);
--- portfolio/script.js
+++ portfolio/script.js
@@ -1,45 +1,54 @@
 $( document).ready(function(){
 
-console.log("testing");
+console.log("Ready to go!");
 
 var i = 0;
 var ticker = 0;
 var count = text.length;
 var scriptPick = text[ticker]["txt"];
-var txt = 'A designer, Sarah Hennessy, 22, a senior at Parsons School of Design waves to the reader.';
-var slug = "IINT. OFFICE - DAY";
+var scriptType = text[ticker]["type"];
+// var txt = 'A designer, Sarah Hennessy, 22, a senior at Parsons School of Design waves to the reader.';
+// var slug = "IINT. OFFICE - DAY";
 var speed = 50;
 var typeSound = new Audio("sound/type.mov");
 
-
-function typeSlug() {
-	document.getElementById("slug").innerHTML += slug.charAt(i);
-	
-    setTimeout(typeSlug, speed, typeEffect);
-}
-
+// console.log(scriptPick, scriptType, ticker);
 
 function typeWriter() {
-    document.getElementById("text").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed, typeEffect);
+    var scriptPick = text[ticker]["txt"];
+    var scriptType = text[ticker]["type"];
+    // console.log(scriptPick, scriptType, ticker);
+    i++; 
+
+    $("#type").append("<p class='typewriter " + scriptType + "''>" + scriptPick + "</p>");
+
 }
 
 function typeEffect() {
 	typeSound.play();
 }
 
+function pageScroll() {
+    window.scrollBy(0,1);
+    scrolldelay = setTimeout(pageScroll,10);
+	}
+
+// 	function pageScroll() {
+//         window.scrollBy(0,50); // horizontal and vertical scroll increments
+//         scrolldelay = setTimeout(pageScroll, 100); // scrolls every 100 milliseconds
+// }
+
 $('button').mousedown(function(e){
 	$(".title").css("opacity", "0");
+	
 	ticker++;
-	console.log(ticker);
 
 	if (ticker >= count){
       ticker = 0;
     }
 
 	typeWriter();
-	typeSlug();
+	pageScroll();
 	});
 
 
